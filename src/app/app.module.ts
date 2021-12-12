7287cbb9a737b83331caca86af8cf0d214441b57
Aman Gupta <aman.gupta@csgi.com>
2021-12-12 12:41:17 +0530

    App header in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -13,7 +13,7 @@ import { MatSliderModule } from '@angular/material/slider';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
