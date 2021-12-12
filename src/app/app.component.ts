bacd57183aa1a7f4b06bc3b741f55963380996ce
Aman Gupta <aman.gupta@csgi.com>
2021-12-12 20:10:57 +0530

    Added savings type and a table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { DataSource } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'financial-portfolio';
+  displayedColumns: any;
+  dataSource: any;
+
+  isItemClicked = false;
+
+  ELEMENT_DATA = [
+    {position: 1, amount: '5000', startDate: '1 Jan 2001', maturityDate: '1 Jan 2006'},
+    {position: 2, amount: '2000', startDate: '7 Feb 2001', maturityDate: '7 Feb 2006'},
+    {position: 3, amount: '400', startDate: '19 July 2021', maturityDate: '19 July 2026'},
+    {position: 4, amount: '5000', startDate: '4 June 2010', maturityDate: '4 June 2015'},
+    {position: 5, amount: '2000', startDate: '21 Apr 2001', maturityDate: '21 Apr 2006'},
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.displayedColumns = ['position', 'amount', 'startDate', 'maturityDate'];
+    this.dataSource = this.ELEMENT_DATA;
+  }
+
+  itemClicked() {
+    this.isItemClicked = true;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import { FormsModule } from '@angular/forms';
+
+import { MatTableModule } from '@angular/material/table'  
+
 
 @NgModule({
   declarations: [
@@ -12,8 +17,14 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTabsModule,
+    MatTableModule
+  ],
+  exports: [
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
