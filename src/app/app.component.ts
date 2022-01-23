3f4380cb8bfebe0ea5f01d61d437953ccd7d945b
Aman Gupta <aman.gupta@csgi.com>
2022-01-23 12:05:08 +0530

    Removed hardcoding for table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { DataSource } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FirebaseGettersService } from './services/firebase-getters.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -11,23 +15,26 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'financial-portfolio';
   displayedColumns: any;
-  dataSource: any;
+  dataSource: any[] = [];
 
   isItemClicked = false;
 
-  ELEMENT_DATA = [
-    {position: 1, amount: '5000', startDate: '1 Jan 2001', maturityDate: '1 Jan 2006'},
-    {position: 2, amount: '2000', startDate: '7 Feb 2001', maturityDate: '7 Feb 2006'},
-    {position: 3, amount: '400', startDate: '19 July 2021', maturityDate: '19 July 2026'},
-    {position: 4, amount: '5000', startDate: '4 June 2010', maturityDate: '4 June 2015'},
-    {position: 5, amount: '2000', startDate: '21 Apr 2001', maturityDate: '21 Apr 2006'},
-  ];
+  public abc: Observable<any>[] | undefined;
+  displayedCols: any = [];
 
-  constructor() {}
+  constructor(firebaseGettersService: FirebaseGettersService) { 
+    firebaseGettersService.getDocument('savingstreams', 'nscList').subscribe(dataSet => {
+      let newArr: any[] = [];
+      let nscList = dataSet.data();
+      _.forOwn(nscList, (val, key) => {
+        newArr.push(val)
+      });
+      this.dataSource = newArr;
+      this.displayedCols = _.keys(this.dataSource[0]);
+    });
+  }
 
   ngOnInit() {
-    this.displayedColumns = ['position', 'amount', 'startDate', 'maturityDate'];
-    this.dataSource = this.ELEMENT_DATA;
   }
 
   itemClicked() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,27 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTabsModule} from '@angular/material/tabs';
 import { FormsModule } from '@angular/forms';
 
-import { MatTableModule } from '@angular/material/table'  
+import { MatTableModule } from '@angular/material/table'; 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FirebaseGettersService } from './services/firebase-getters.service';
+import { TableDisplayComponent } from './components/table/table-display.component';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyBM_65aCQtS2TOI7KaJN5LT7dsr4idYPdM",
+  authDomain: "fportfolio.firebaseapp.com",
+  databaseURL: "https://fportfolio-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fportfolio",
+  storageBucket: "fportfolio.appspot.com",
+  messagingSenderId: "1041143133928",
+  appId: "1:1041143133928:web:114384f7485454b62e3b5d",
+  measurementId: "G-281QYW9DCD"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableDisplayComponent
   ],
   imports: [
     BrowserModule,
@@ -21,12 +36,16 @@ import { MatTableModule } from '@angular/material/table'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatTabsModule,
-    MatTableModule
+    MatTableModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   exports: [
     MatTabsModule
   ],
-  providers: [],
+  providers: [
+    FirebaseGettersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
